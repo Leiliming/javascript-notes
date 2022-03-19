9bc6151e99880d5cd6ba80df8e548654d0c6e189
力铭 <38111405+Leiliming@users.noreply.github.com>
2022-03-19 15:06:47 +0800

    手写all、resolve

--- promise/index.js
+++ promise/index.js
@@ -1,20 +1,19 @@
-// const MyPromise = require('./myPromise')
-// function p1() {
-//     return new MyPromise(function (resolve, reject) {
-//         setTimeout(function () {
-//             resolve('p1')
-//         }, 2000)
-//     })
-// }
+const MyPromise = require("./myPromise");
+function p1() {
+  return new MyPromise(function (resolve, reject) {
+    setTimeout(function () {
+      resolve("p1");
+    }, 2000);
+  });
+}
 // function p2() {
 //     return new MyPromise(function (resolve, reject) {
 //         resolve('p2')
 //     })
 // }
 
-
-
 // MyPromise
 //     .all(['a', 'b', p1(), p2(), 'c'])
 //     .then(result => console.log(result), reason => console.log(reason))
-
+MyPromise.resolve("10").then(value => console.log(value));
+MyPromise.resolve(p1()).then(value => console.log(value));
